048c8163a1e85746110977af633cc0e17891c899
Jeferson Ferreira <jefersonpks@gmail.com>
2022-07-14 20:35:09 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Te amo Minha Princesa!</h1>
-      <NotificationButton />
+      <Header/>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard/>
+        </div>
+      </section>
     </>
   )
 }
