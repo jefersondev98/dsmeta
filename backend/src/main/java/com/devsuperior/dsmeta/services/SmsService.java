f24a741210dd05941aabe00f8acbe6711a68bf32
Jeferson Ferreira <jefersonpks@gmail.com>
2022-07-15 02:08:37 -0300

    Reset message after sendSms

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -44,6 +44,6 @@ public class SmsService {
 
 		Message message = Message.creator(to, from, msg).create();
 
-		System.out.println(message.toString());
+		System.out.println(message.getSid());
 	}
 }
